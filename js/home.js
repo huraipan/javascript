3df3dcb16e7e89ed1579ce0feb60c21cc810f085
Park <106230148+huraipan@users.noreply.github.com>
2023-06-29 17:26:26 +0900

    b
    b

--- js/home.js
+++ js/home.js
@@ -1,10 +1,18 @@
-$.get().done((products) => {
-  products.forEach((a, i) => {
-    var 템플릿 = `<div class="col-sm-4">
-        <img src="${products[i].photo}" class="w-100">
-        <h5>${products[i].title}</h5>
-        <p>가격 : ${products[i].price}</p>
+$.getJSON(
+  "https://raw.githubusercontent.com/huraipan/javascript/main/store.json",
+  function (data) {
+    var products = data.products;
+
+    products.forEach((product) => {
+      var 템플릿 = `<div class="item">
+        <img src="img/${product.photo}" class="w-100">
+        <h5>${product.title}</h5>
+        <p>${product.brand}</p>
+        <p>가격: ${product.price}</p>
+        
+        <button class="cart-btn">담기</button>
       </div>`;
-    $(".items").append(템플릿);
-  });
-});
+      $(".items").append(템플릿);
+    });
+  }
+);
